68811629452a90dbeaa2eb46e039d8a08979aca1
Ioseph Kim <iosephkim@gmail.com>
2025-04-11 05:49:44 +0900

    Update pico-soundcard.ino

--- pico-soundcard.ino
+++ pico-soundcard.ino
@@ -1,73 +1,94 @@
-// from chatgpt
-#include <Arduino.h>
-#include "tusb.h"  // TinyUSB 라이브러리
-#include "hardware/i2s.h"
-#include "pico/stdlib.h"
-#include "pico/audio_i2s.h"
+/*********************************************************************
+ Adafruit invests time and resources providing this open source code,
+ please support Adafruit and open-source hardware by purchasing
+ products from Adafruit!
 
-#define I2S_DATA_OUT 9   // I2S 출력 데이터 핀 (DAC)
-#define I2S_DATA_IN 12   // I2S 입력 데이터 핀 (MIC)
-#define I2S_BCK 10       // I2S 비트 클럭
-#define I2S_LRCK 11      // I2S 좌우 클럭
+ MIT license, check LICENSE for more information
+ Copyright (c) 2024 Phl Schatzmann
 
-#define AUDIO_SAMPLE_RATE 44100  // 44.1kHz 샘플링 레이트
-#define AUDIO_BUFFER_SIZE 512    // 버퍼 크기
+ This example uses a Rasperry Pico to send the output to i2s. To prevent
+ any underruns or overruns we take care of the feedback ourself. 
+ 
+*********************************************************************/
 
-// I2S 오디오 출력 설정
-audio_buffer_pool_t *init_audio_i2s() {
-    static audio_format_t audio_format = {
-        .sample_freq = AUDIO_SAMPLE_RATE,
-        .format = AUDIO_BUFFER_FORMAT_PCM_S16,
-        .channel_count = 2
-    };
-    
-    static i2s_config_t i2s_config = {
-        .data_pin = I2S_DATA_OUT,
-        .clock_pin_base = I2S_BCK,
-        .dma_channel = 0,
-        .pio_sm = 0
-    };
-    
-    audio_buffer_pool_t *pool = audio_i2s_setup(&audio_format, &i2s_config);
-    return pool;
+/***************
+    징하게 오래 걸렸네
+    1. Adafruit_TinyUSB 필요함
+    2. 그런데, 여긴 audio가 없음
+    3. https://github.com/pschatzmann/Adafruit_TinyUSB_Arduino/tree/Audio 어르신이 만들었음 
+    4. 소리가 끊긴다고 예제 코드에 큐를 사용했는데, 이 큐가 나의 환경에서는 정상 작동 안됨 
+    5. 큐 다 빼고 시리얼 디버그 다 빼고 코드 정리해도 안됨
+    6. i2s data pin 지정을 해야 했음
+    7. 다 되었는데, 스테레오의 좌우가 바뀌어 들림 (dac 보드 문제인지, 코드 문제인지는 아직 확인 안했음)
+***************/
+
+#include "Adafruit_TinyUSB.h"
+#include "AudioTools.h"
+#include "AudioTools/Concurrency/RP2040.h"
+
+AudioInfo info(44100, 2, 16);
+Adafruit_USBD_Audio usb;
+BufferRP2040 buffer(256, 20); 
+//QueueStream queue(buffer);
+I2SStream i2s;
+//StreamCopy copier(i2s, queue);
+
+size_t writeCB(const uint8_t* data, size_t len, Adafruit_USBD_Audio& ref) {
+  usb.setFeedbackPercent(buffer.size()*100 / buffer.available());
+  //Serial.println(len);
+
+  // Serial.println(data);
+  return i2s.write(data,len);
 }
 
-// I2S 마이크 입력 설정
-void init_i2s_mic() {
-    i2s_inst_t *i2s = i2s0;
-    i2s_configure(i2s, I2S_BCK, I2S_LRCK, I2S_DATA_IN);
-    i2s_start(i2s);
-}
+void setup() {  
+  //Serial.begin(115200);
+  //AudioToolsLogger.begin(Serial, AudioToolsLogLevel::Warning);
+  //while(!Serial);  // wait for serial
+  //delay(1000);
+  //Serial.println("starting...");
+  // start queue
+  //queue.begin(80);
 
-// USB 오디오 출력 콜백
-void tud_audio_tx_done_cb(uint8_t rhport, uint8_t ep_out, uint8_t ep_in) {
-    static int16_t audio_buffer[AUDIO_BUFFER_SIZE];
-    size_t received_bytes = tud_audio_read(audio_buffer, sizeof(audio_buffer));
-    
-    if (received_bytes > 0) {
-        struct audio_buffer *buffer = take_audio_buffer(init_audio_i2s(), true);
-        if (buffer) {
-            memcpy(buffer->buffer, audio_buffer, received_bytes);
-            buffer->sample_count = received_bytes / 2;
-            give_audio_buffer(init_audio_i2s(), buffer);
-        }
-    }
-}
+  // Start USB device as Audio Sink w/o automatic feedback
+  usb.setFeedbackMethod(0);
+  usb.setWriteCallback(writeCB);
+  if (!usb.begin(info.sample_rate, info.channels, info.bits_per_sample)){
+    // Serial.println("USB error");
+    int i = 0;
+  }
 
-// USB 마이크 입력 콜백
-void tud_audio_rx_done_cb(uint8_t rhport, uint8_t ep_out, uint8_t ep_in) {
-    static int16_t mic_buffer[AUDIO_BUFFER_SIZE];
-    i2s_read_blocking(i2s0, mic_buffer, sizeof(mic_buffer));
-    tud_audio_write(mic_buffer, sizeof(mic_buffer));
-}
+  // If already enumerated, additional class driverr begin() e.g msc, hid, midi won't take effect until re-enumeration
+  if (TinyUSBDevice.mounted()) {
+    TinyUSBDevice.detach();
+    delay(10);
+    TinyUSBDevice.attach();
+  }
 
-void setup() {
-    Serial.begin(115200);
-    tud_init(BOARD_TUD_RHPORT);
-    init_audio_i2s();
-    init_i2s_mic();
+  
 }
 
 void loop() {
-    tud_task(); // TinyUSB 이벤트 처리
+  //usb.updateLED();
+}
+
+void setup1(){
+  //start i2s
+  auto cfg = i2s.defaultConfig(TX_MODE);
+  // 이거 지정해야 소리남
+  cfg.pin_data = 28;
+  // 기본 핀 bck = 26, ws = 27
+  cfg.copyFrom(info);
+  cfg.buffer_size = 256;
+  cfg.buffer_count = 3;
+  if (!i2s.begin(cfg)){
+    // Serial.print("i2s error");
+    int i  = 0;
+  }
+}
+
+void loop1() {
+  //Serial.print('call');
+  //Serial.println(queue.
+  // copier.copy();
 }
